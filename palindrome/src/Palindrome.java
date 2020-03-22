77295b1de3442749c38e6e82e0aa78cd2cb1e3f6
szpa3ek <szpa3ek@gmail.com>
2020-03-22 23:45:14 +0100

    second task

---
+++ palindrome/src/Palindrome.java
@@ -0,0 +1,29 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int max = 0;
+        int firstNumber = 0;
+        int secondNumber = 0;
+
+        for (int i = 999; i > 100; i--) {
+            for (int j = i; j > 100; j--) {
+                int multiplication = i * j;
+                if (isPalindrome(multiplication)) {
+                    if ((i + j) > max) {
+                        max = i + j;
+                        firstNumber = i;
+                        secondNumber = j;
+                    }
+                }
+            }
+        }
+        System.out.println("first number: " + firstNumber + " second number: " + secondNumber);
+        System.out.println(firstNumber * secondNumber);
+    }
+
+    private static boolean isPalindrome(int number) {
+        String p = Integer.toString(number);
+        StringBuilder s = new StringBuilder(Integer.toString(number));
+        s.reverse();
+        return p.equals(s.toString());
+    }
+}
