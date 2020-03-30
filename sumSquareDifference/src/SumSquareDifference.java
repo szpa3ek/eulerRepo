74d25413368fd6012da9a0fbe20c2c602dfb1f5f
szpa3ek <szpa3ek@gmail.com>
2020-03-30 21:26:27 +0200

    sumSquareDifference

---
+++ sumSquareDifference/src/SumSquareDifference.java
@@ -0,0 +1,13 @@
+public class SumSquareDifference {
+
+    public static void main(String[] args) {
+        long sumOfSqrt = 0L;
+        long sqrtOfSum = 0L;
+        for (int i = 1; i <= 100; i++) {
+            sumOfSqrt += Math.pow(i, 2);
+            sqrtOfSum += i;
+        }
+        long powerOfSum = (long) Math.pow(sqrtOfSum, 2);
+        System.out.println(powerOfSum - sumOfSqrt);
+    }
+}
