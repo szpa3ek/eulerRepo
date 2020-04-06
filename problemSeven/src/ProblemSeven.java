5c391ce337d175d9eb740b35035584b1d86e7a8f
szpa3ek <szpa3ek@gmail.com>
2020-04-06 21:34:40 +0200

    added ProblemSeven

---
+++ problemSeven/src/ProblemSeven.java
@@ -0,0 +1,29 @@
+public class ProblemSeven {
+
+    public static void main(String[] args) {
+        int position = 10001;
+        int counter = 1;
+        int primeNum = 0;
+
+        primeNum = getPrimeNumber(position, counter, primeNum);
+        System.out.println("10001st prime number is: " + primeNum);
+    }
+
+    private static int getPrimeNumber(int position, int counter, int primeNum) {
+        for (int i = 2; counter <= position; i++) {
+            if (isPrime(i)) {
+                counter++;
+                primeNum = i;
+            }
+        }
+        return primeNum;
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
+
